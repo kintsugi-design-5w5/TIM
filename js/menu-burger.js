a9764ea7b9f66faf3e93fb8dbfcc4e6d767d9ae7
Dylan-Carle <dylancarle2005@hotmail.com>
2024-09-23 14:59:03 -0400

    menu nav fait plus debut parallax

--- js/menu-burger.js
+++ js/menu-burger.js
@@ -4,7 +4,6 @@ let burger = document.querySelector("#burger");
 burger.addEventListener("click", ClickBurger);
 
 function ClickBurger(){
-    console.log(burger.checked)
     if(burger.checked){
         document.querySelector('body').style.overflowY = 'hidden';
         console.log(window.scrollY); //Donne la position du scroll
