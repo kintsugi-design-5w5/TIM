450978ec5d9f741b34b25084a6ecbece8fc1bd70
joejoenachos <119699978+joejoenachos@users.noreply.github.com>
2024-10-02 10:48:57 -0400

    2024-10-02 Avancement du style global de l'accueil

--- js/menu-burger.js
+++ js/menu-burger.js
@@ -1,14 +1,12 @@
-
 let burger = document.querySelector("#burger");
 
 burger.addEventListener("click", ClickBurger);
 
-function ClickBurger(){
-    if(burger.checked){
-        document.querySelector('body').style.overflowY = 'hidden';
+function ClickBurger() {
+    if (burger.checked) {
+        document.querySelector("body").style.overflowY = "hidden";
         console.log(window.scrollY); //Donne la position du scroll
-    }
-    else{
-        document.querySelector('body').style.overflowY = 'auto';
+    } else {
+        document.querySelector("body").style.overflowY = "auto";
     }
 }
