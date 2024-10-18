fa6f4a69cfb7773dad8842ae8431955f332396df
Dylan-Carle <119865209+Dylan-Carle@users.noreply.github.com>
2024-10-18 01:54:19 -0400

    2024-10-18 optimisation du hover pour les masks

--- js/cercle-avenir.js
+++ js/cercle-avenir.js
@@ -4,13 +4,27 @@ let lesConteneurMask = document.querySelectorAll(".conteneur-mask");
 for(let i = 0; i < lesConteneurMask.length; i++){
     lesConteneurMask[i].addEventListener("mouseover", function(){
         let leMask = lesConteneurMask[i].children[0].nextSibling;
-        leMask.style.zIndex = 2;
+        leMask.style.zIndex = 4;
+
+        lesConteneurMask[i].children[0].style.zIndex = 5;
+
+        for(let j = 0; j < lesConteneurMask.length; j++){
+            if(lesConteneurMask[j] != lesConteneurMask[i]){
+                lesConteneurMask[j].style.pointerEvents = "none";
+            }
+        }
     });
 }
 
 for(let i = 0; i < lesConteneurMask.length; i++){
     lesConteneurMask[i].addEventListener("mouseout", function(){
         let leMask = lesConteneurMask[i].children[0].nextSibling;
-        setTimeout(function(){leMask.style.zIndex = 1;}, 500);
+        setTimeout(function(){
+            for(let i = 0; i < lesConteneurMask.length; i++){
+                lesConteneurMask[i].style.pointerEvents = "auto";
+                lesConteneurMask[i].children[0].style.zIndex = 3;
+            }
+            leMask.style.zIndex = 1;
+        }, 500);
     });
 }
