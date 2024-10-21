158ba5b8f493dcf654e03561220181e5ab555bc3
Samuel Giroux <s_giroux@icloud.com>
2024-10-21 14:33:02 -0400

    Création des pages php pour Stages, Emplois et Universités

---
+++ universites.php
@@ -0,0 +1,32 @@
+<?php 
+    get_header();
+?>
+        <main class="sous-section">
+            <div>
+                <a>Retour</a>
+            </div>
+            <div class="section-universites">
+            <div>
+                <h2>UNIVERSITÉS</h2>
+            </div>
+            <div>
+                <p>
+                    Un DEC+BAC permet aux finissants du programme TIM de poursuivre leurs études à l'université 
+                    afin de se faire créditer des cours sous certaines conditions. Le nombre de crédits varie selon le 
+                    programme choisi. De plus, les étudiants ont accès à tous les cours dont le préalable est un DEC.
+                </p>
+            </div>
+            <div>
+                <ul>
+                    <li>DEC-BAC en création numérique - concentration Cinéma, Création 3D ou Technologie Web à l’UQAT</li>
+                    <li>DEC-BAC en création de jeux vidéo - Intégration logicielle à l’UQAT</li>
+                    <li>B. en communication (création médias – médias interactifs) à l’UQAM</li>
+                    <li>Bac en génie des technologies de l’information ou en génie logiciel à l’ÉTS</li>
+                </ul>
+            </div>
+        </div>
+        </main>
+        <?php
+            get_footer();
+        ?>
+</html>
