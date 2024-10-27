91893b82a39eafed3870a928e6347fd1c0dc2b64
Minji Ardanuy-Jetté <minjiajette@gmail.com>
2024-10-26 22:45:57 -0400

    2024-10-26 Version 1 du scroll de la progression de la timeline

--- js/barre-progression-cours.js
+++ js/barre-progression-cours.js
@@ -1,9 +1,46 @@
-//Variables des éléments de la barre de progression
-
+// Variables pour les éléments de la barre de progression
 let timeline = document.querySelector(".timeline");
 let barreProgression = document.querySelector(".barre-progression");
+let hauteurTimeline = timeline.offsetHeight;
 
-function MiseAjourBarreProgression() {}
+// Variables de la page
+let hauteurFenetre = window.innerHeight; // Obtenir la hauteur de la fenêtre visible
 
-//Ajout d'un écouteur d'événement au scroll sur la page
-document.addEventListener("scroll", MiseAjourBarreProgression);
+// Variable de la hauteur de la section hero en pourcentage
+let hauteurSectionHero = 100;
+
+// Fonction pour mettre à jour la barre de progression en fonction du scroll
+function mettreAJourBarreProgression() {
+  // Calculer la position de défilement par rapport au haut de la page
+  let positionDefilement = window.scrollY; // Mettre à jour ici pour avoir la position actuelle
+
+  // Calculer la hauteur totale défilable du document
+  let hauteurDocument = document.body.scrollHeight; // Hauteur totale du document
+  let hauteurDefilable = hauteurDocument - hauteurFenetre; // Hauteur défilable, soit la section des cours
+
+  // Début de la barre de progression après la section hero
+  let defilementTimeline = (hauteurSectionHero * hauteurFenetre) / 100;
+
+  // Vérifier si la position de défilement est au-delà de la section hero
+  if (positionDefilement > defilementTimeline) {
+    // Calculer le pourcentage de défilement dans la zone de la timeline
+    // Math.min limite le pourcentage de défilement à 1 (100%) maximum
+    let pourcentageDefilement = Math.min(
+      (positionDefilement - defilementTimeline) /
+        (hauteurDefilable - defilementTimeline),
+      1
+    );
+
+    // Calculer la hauteur de la barre de progression en fonction du pourcentage de défilement
+    let nouvelleHauteur = pourcentageDefilement * hauteurTimeline;
+
+    // Mettre à jour la hauteur de la ligne de progression
+    barreProgression.style.height = `${nouvelleHauteur}px`;
+  } else {
+    // Réinitialiser la hauteur de la barre de progression si le défilement est dans la section hero
+    barreProgression.style.height = "0px";
+  }
+}
+
+// Ajouter un écouteur d'événement pour déclencher la fonction au scroll
+document.addEventListener("scroll", mettreAJourBarreProgression);
