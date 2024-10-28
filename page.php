1676ab1bd1851fcdc3e7089ff78fe827dc5a8430
Samuel Giroux <s_giroux@icloud.com>
2024-10-28 12:45:30 -0400

    Title retiré dans page.php

--- page.php
+++ page.php
@@ -7,7 +7,7 @@
             if(have_posts()){
                 while(have_posts()){
                     the_post();
-                    the_title();
+                    //the_title();
                     the_content();
                 }
             }
