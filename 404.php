4148e7ff6159136d7dad25c3092716403d93a34d
Dylan-Carle <dylancarle2005@hotmail.com>
2024-11-11 12:48:57 -0500

    11-11-2024 commit pour merge

--- 404.php
+++ 404.php
@@ -2,8 +2,12 @@
     get_header();
 ?>
 
-<section class="flex">
-    <h1>Silence is golden</h1>
+<section class="section-404 flex">
+    <div class="div-404 flex">
+        <h1>Erreur 404</h1>
+        <p>Vous entrez dans un santier non-batu du TIM. </p>
+        <p>Le vide est partout ...</p>
+    </div>
 </section>
 
 <?php 
