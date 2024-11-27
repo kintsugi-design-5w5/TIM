f3ebe1d97e56e70ebf0e385f9a95902053957912
Minji Ardanuy-Jetté <minjiajette@gmail.com>
2024-11-27 10:25:54 -0500

    2024-11-27 Ajout du par an pour le nombre en dollars

--- js/graphique-animation.js
+++ js/graphique-animation.js
@@ -34,7 +34,7 @@ document.addEventListener("DOMContentLoaded", () => {
           case "dollars":
             elementNombre.innerHTML = `${pourcentageActuel
               .toFixed(2)
-              .replace(".", ",")}$`;
+              .replace(".", ",")} $/an`;
             break;
         }
       } else {
