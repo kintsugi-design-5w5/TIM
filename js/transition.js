9b77e8c793d3024c17a660e9cd721d63ca4e9aa9
Minji Ardanuy-Jetté <minjiajette@gmail.com>
2024-12-04 09:09:16 -0500

    2024-12-04 Dupprimer le code en commentaire dans le javascript

--- js/transition.js
+++ js/transition.js
@@ -1,28 +1,18 @@
-
-// transitionToPage = function(href) {
-//     document.querySelector('.chargement').classList.remove('invisible');
-//     document.querySelector('.chargement').classList.add('visible');
-//     document.querySelector("body").style.overflowY = "hidden";
-
-//     setTimeout(function() {
-//         window.location.href = href;
-//     }, 2000);
-// }
-let chargement = document.querySelector('.chargement');
+let chargement = document.querySelector(".chargement");
 
 function transitionToPage(href) {
-    chargement.classList.remove('invisible');
-    chargement.classList.add('visible');
-    document.querySelector("body").style.overflowY = "hidden";
+  chargement.classList.remove("invisible");
+  chargement.classList.add("visible");
+  document.querySelector("body").style.overflowY = "hidden";
 
-    setTimeout(function() {
-        window.location.href = href;
-    }, 1000);
+  setTimeout(function () {
+    window.location.href = href;
+  }, 1000);
 }
 
-document.addEventListener('DOMContentLoaded', function() {
-    console.log('DOM chargé');
-    chargement.classList.remove('visible');
-    chargement.classList.add('invisible');
-    document.querySelector("body").style.overflowY = "auto";
+document.addEventListener("DOMContentLoaded", function () {
+  console.log("DOM chargé");
+  chargement.classList.remove("visible");
+  chargement.classList.add("invisible");
+  document.querySelector("body").style.overflowY = "auto";
 });
